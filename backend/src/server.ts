d9eb96a2ed5bc807bc29d611e78d48d0ec6327a7
Deepufrancis <deepufrancis999@gmail.com>
2025-12-09 14:01:55 +0530

    final changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,10 +11,16 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://busbook-1-pr1x.onrender.com"
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
